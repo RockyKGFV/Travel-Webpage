a232abec9bd0f635b2695475452ec15e0dbdfcf0
RockyKGFV <118094122+RockyKGFV@users.noreply.github.com>
2024-03-17 22:46:11 +0530

    reportWebVitals.js

---
+++ reportWebVitals.js
@@ -0,0 +1,14 @@
+const reportWebVitals = onPerfEntry => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      // Call each function to measure specific web vitals and pass the result to onPerfEntry function
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
